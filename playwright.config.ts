a63cfb70a86d58c0c271577381edf7708cf1dff1
m-caicedo <macaicedo03@gmail.com>
2026-06-20 22:38:45 -0500

    fix: playwright configs and package json

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig, devices } from '@playwright/test';
 import { AllureReporter } from 'allure-playwright';
+import dotenv from 'dotenv';
+
+dotenv.config({path: 'env/.env'});
 
 /**
  * Read environment variables from file.
@@ -21,13 +24,13 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 4 : 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [["allure-playwright"],["html", {outputFolder: "playwright-report", open: "never"}], ["list"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: 'https://automationexercise.com/',
+    baseURL: process.env.URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
